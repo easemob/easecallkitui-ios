74f75000d82d8bc0ae5ae0c46f3972e62958df54
dujiepeng <347302029@qq.com>
2021-02-18 20:07:11 +0800

    add podspec file

---
+++ EaseCallDemo/EaseCallDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  EaseCallDemo
+//
+//  Created by 杜洁鹏 on 2021/2/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
